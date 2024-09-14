b6f053c67308bd80efaab3ec84718ac3ba21a06f
Resterr1337 <Franklion1chance@gmail.com>
2024-09-14 21:03:33 +0500

    SearchInputFixes

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -4,10 +4,9 @@ import { useState } from "react";
 import "./SearchInput.css";
 import SearchSVG from "@/assets/SearchInput/search.svg?react";
 
-const SearchInput = ({prevValue}) => {
-	const [value , setValue] = useState(prevValue || "")
+const SearchInput = ({ prevValue }) => {
+	const [value, setValue] = useState(prevValue || "");
 	const NavigateFunc = useNavigate();
-
 
 	return (
 		<>
@@ -15,39 +14,59 @@ const SearchInput = ({prevValue}) => {
 				sx={{
 					display: "flex",
 					alignItems: "center",
-					justifyContent: "space-between",
+					justifyContent: "start",
 				}}
 			>
 				<SearchSVG></SearchSVG>
-				<Input
-					onInput={() => {
-						setValue(event.target.value)
+				<form
+					onSubmit={(event) => {
+						event.preventDefault();
+						value
+						? NavigateFunc("/search/" + value)
+						: NavigateFunc("/categories");
 					}}
-					onBlur={() => {
-						const inputValue = event.target.value;
-						{inputValue? NavigateFunc("/search/" + inputValue): NavigateFunc("/categories")}
+					style={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "start",
 					}}
-					sx={{
-						ml: "1rem",
-						py: "0.75rem",
-						fontFamily: "Onest , sans-serif",
-						fontSize: "1rem",
-						fontWeight: "400",
-						color: "#1F2024",
-						"&::placeholder": {
-							color: "black",
-						},
-						"&&&:before": {
-							borderBottom: "none",
-						},
-						"&&:after": {
-							borderBottom: "none",
-						},
-					}}
-					fullWidth={true}
-					placeholder="Поиск товаров"
-					value={value}
-				></Input>
+				>
+					<Input
+						onInput={() => {
+							setValue(event.target.value);
+						}}
+						onBlur={() => {
+							const inputValue = event.target.value;
+							{
+								inputValue
+									? NavigateFunc("/search/" + inputValue)
+									: NavigateFunc("/categories");
+							}
+						}}
+						sx={{
+							ml: "1rem",
+							py: "0.75rem",
+							fontFamily: "Onest , sans-serif",
+							fontSize: "1rem",
+							fontWeight: "400",
+							color: "#1F2024",
+							"&::placeholder": {
+								color: "black",
+							},
+							"&&&:before": {
+								borderBottom: "none",
+							},
+							"&&:after": {
+								borderBottom: "none",
+							},
+						}}
+						fullWidth={true}
+						placeholder="Поиск товаров"
+						value={value}
+					></Input>
+
+					<input style={{display:"none"}} type="submit" value="" />
+				</form>
 			</Box>
 		</>
 	);
